9e196192793503ff1b863a0fba09152328f3d7ac
Alejandro Chávez Campos <A01374974@itesm.mx>
2020-04-06 17:06:25 -0500

    Assigment 4

--- Assigment4/threejsInteraction.js
+++ Assigment4/threejsInteraction.js
@@ -8,13 +8,21 @@ let objLoader = null, jsonLoader = null;
 
 let fract = 0;
 let deltat = 1000;
-let duration = 20000; // ms
+let duration = 200000; // ms
 let currentTime = Date.now();
+let seconds=0;
+let secondFinal=59;
 let directionalLight = null;
 let spotLight = null;
 let ambientLight = null;
 let pointLight = null;
+let minutes=3
+let timer=59
+let gameOver=false;
+let puntuacion=0;
+let salud=50;
 
+let now = new Date().getTime();
 let objElefantlUrl = {obj:'../models/obj/elefante/elefante.obj', map:'../models/obj/elefante/Textures/elefantefull.png', normalMap:'../models/obj/elefante/Textures/elefantefull.png', specularMap: '../models/obj/elefante/Textures/elefantefull.png'};
 
 let objModelUrl = {obj:'../models/obj/leon/12273_Lion_v1_l3.obj', map:'../models/obj/leon/12273_Lion_Diffuse.jpg',normalMap:'../models/obj/leon/12273_Lion_Diffuse.jpg', specularMap: '../models/obj/leon/12273_Lion_Diffuse.jpg'};
@@ -102,7 +110,8 @@ async function loadObj(objModelUrl, objectList)
     try {
         let texture = objModelUrl.hasOwnProperty('map') ? new THREE.TextureLoader().load(objModelUrl.map) : null;
         let normalMap = objModelUrl.hasOwnProperty('normalMap') ? new THREE.TextureLoader().load(objModelUrl.normalMap) : null;
-        for ( let i = 0; i < 2; i ++ )  {
+        
+        for ( let i = 0; i < 6; i ++ )  {
             const object = await objPromiseLoader.load(objModelUrl.obj);
             
             
@@ -116,7 +125,7 @@ async function loadObj(objModelUrl, objectList)
             });
 
             object.scale.set(8, 8, 8);
-            object.name = 'leon';
+            object.name = 'elefante';
             //object.position.set(60,60,-183)
             //object.position.set(Math.floor(Math.random()*(90+90))-90, Math.floor(Math.random()*(100+90))-90, -183);
             object.position.set(window.innerWidth/9, Math.floor(Math.random()*(100+90))-90, -183);
@@ -126,9 +135,9 @@ async function loadObj(objModelUrl, objectList)
             
             scene.add(object);
         }
-        numeroLeon=Math.floor(Math.random()*(4+2))+2
-        console.log(numeroLeon)
-        for ( let i = 0; i < numeroLeon; i ++ )  {
+       
+       
+        for ( let i = 0; i <5; i ++ )  {
             const object = await objPromiseLoader.load(objModelUrl.obj);
             
             
@@ -152,31 +161,7 @@ async function loadObj(objModelUrl, objectList)
             
             scene.add(object);
         }
-        let numeroLeon2=Math.floor(Math.random()*(4+2))+2
-        for ( let i = 0; i < numeroLeon2; i ++ )  {
-            const object = await objPromiseLoader.load(objModelUrl.obj);
-            
-            
-            object.traverse(function (child) {
-                if (child instanceof THREE.Mesh) {
-                    child.castShadow = true;
-                    child.receiveShadow = true;
-                    child.material.map = texture;
-                    child.material.normalMap = normalMap;
-                }
-            });
-
-            object.scale.set(8, 8, 8);
-            object.name = 'elefante';
-            //object.position.set(60,60,-183)
-            //object.position.set(Math.floor(Math.random()*(90+90))-90, Math.floor(Math.random()*(100+90))-90, -183);
-            object.position.set(Math.floor(Math.random()*(90+90))-90, 90, -183);
-            console.log(object.position)
-            objectList.push(object);
-            console.log()
-            
-            scene.add(object);
-        }
+       
     }
     catch (err) {
         return onError(err);
@@ -185,8 +170,10 @@ async function loadObj(objModelUrl, objectList)
 function animate() {
 
     let now = Date.now();
-    let deltat = now - currentTime;
-    currentTime = now;
+    //let deltat = now - currentTime;
+    //currentTime = now;
+    
+    
     let fract = deltat / duration;
     let angle = Math.PI * fract;
 
@@ -221,11 +208,16 @@ function animate() {
                 }else if(object.position.x <= 0){
                     object.position.x+=0.1;
                 }
+                
+
             }
             
+                
             
-        
+            
+            
         }
+
 
 }
 function run() 
@@ -341,14 +333,21 @@ function onDocumentMouseDown(event)
     // find intersections
     raycaster.setFromCamera( mouse, camera );
 
-    let intersects = raycaster.intersectObjects( scene.children );
+    let intersects = raycaster.intersectObjects( scene.children,true );
 
-    console.log("intersects", intersects);
+    //console.log("intersects", intersects);
     if ( intersects.length > 0 ) 
     {
         CLICKED = intersects[ intersects.length - 1 ].object;
-        CLICKED.material.emissive.setHex( 0x00ff00 );
-        console.log(CLICKED.name);
+        if(!gameOver&&  CLICKED.name=="mesh_0"){
+        //CLICKED.material.emissive.setHex( 0x00ff00 );
+        //console.log(CLICKED.name);
+        CLICKED.parent.alive=false;
+
+        CLICKED.parent.position.set(window.innerWidth/6, Math.floor(Math.random()*(100+90))-90, -183)
+        puntuacion+=1
+       
+        $("#score").html("SCORE: " + puntuacion );}
     } 
     else 
     {
@@ -356,15 +355,55 @@ function onDocumentMouseDown(event)
             CLICKED.material.emissive.setHex( CLICKED.currentHex );
 
         CLICKED = null;
+        
     }
 }
-//
+//Función run correcta
 function run() 
 {
+    if (gameOver){
+     
+     $("#reiniciar").html("<h4><A HREF='javascript:history.go(0)' style='color:BLACK'>REINICIAR</A></h4>"); 
+     
+    }else{
     requestAnimationFrame( run );
     render();
     animate()
+    setTimeout(temporizador,100)
+    }
 }
+function habilitarBoton(){
+    let boton1 = document.getElementById("boton1"); 
+    boton1.disabled = false;
+}
+
+function temporizador(){
+    
+        if(secondFinal==0){
+            gameOver=true
+        }
+        /*if(minutes === 0 && timer < 0){
+            //gameover = true;
+        }else{
+            if(timer<0){
+                minutes -= 1;
+                timer = 59
+            }
+            timer -= 0.1;
+            
+            $("#timer").html("<h1>0" + minutes +":"+
+            ("0" + Math.floor(timer.toFixed(1))).slice(-2) + "</h1>" );
+        }
+        console.log(timer)*/
+        else{
+        seconds+=1/90
+            if(seconds>=1){
+                secondFinal-=1
+                seconds=0
+            }
+            $("#timer").html("<h1>0 :"+
+            ( " "+ secondFinal) + "</h1>" );}
+     }
 
 function render() 
 {
